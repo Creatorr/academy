afcad36ea1daf77162af051bd034d3b2d0aa86b7
AR <romnospam@gmail.com>
2015-09-05 10:31:37 +0500

    refactor codeeval Chardonnay

--- python/2015_08_CODEEVAL_Chardonnay or Cabernet.py
+++ python/2015_08_CODEEVAL_Chardonnay or Cabernet.py
@@ -14,20 +14,28 @@
 
 import sys
 
+#  import urllib2
+#  test  = https://github.com/Creatorr/academy/blob/master/test_for_scripts/test_2015_08_CODEEVAL_Chardonnay%20or%20Cabernet.txt
+#  test_cases = urllib2.urlopen("https://dl.dropboxusercontent.com/u/31749115/test.txt")
+
 test_cases = open(sys.argv[1], 'r')
+
 for test in test_cases:
-    # test = "Cabernet Merlot Noir | ot"
-    # test = "Chardonnay Sauvignon | ann"
-    # test = "Shiraz Grenache | o"
     if len(test) > 0:
         pos_sep = test.find('|')
-        wine = test[:pos_sep - 1]
-        letters = test[pos_sep + 2:]
-        for letter_wine in wine:
-            if letters.find(letter_wine) > -1:
-                letters = letters.replace(letter_wine, "")
-        if len(letters) > 0:
-            print False
+        wine_str = test[:pos_sep - 1]
+        wine_list = wine_str.split(" ")
+        wine_out = list()
+        letters = test[pos_sep + 2:].strip()
+        for wine in wine_list:
+            letters_temp = letters
+            for letter_wine in wine:
+                if letters_temp.find(letter_wine) > -1:
+                    letters_temp = letters_temp.replace(letter_wine, "", 1)
+            if len(letters_temp) == 0:
+                wine_out.append(wine)
+        if len(wine_out) > 0:
+            print " ".join(wine_out)
         else:
-            print wine
+            print False
 test_cases.close()
